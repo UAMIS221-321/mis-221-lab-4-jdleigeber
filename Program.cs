c984eb23844391fa3deac6525e1203424cf40094
Jacob Leigeber <jdleigeber@crmson.ua.edu>
2024-09-30 10:17:54 -0500

    Lab4\

--- Program.cs
+++ Program.cs
@@ -1,2 +1,122 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userInput = PizzaMenuChoice();
+
+while(userInput != "4"){
+    RouteEm(userInput);
+    userInput = PizzaMenuChoice();
+}
+ExitSequence();
+
+static string PizzaMenuChoice(){
+    Console.Clear();
+    Console.WriteLine("Enter 1 to display a plain topping-less pizza\nEnter 2 to display cheese pizza slice\nEnter 3 to display pepperoni pizza slice\nEnter 4 to exit");
+    Console.Write("Enter your choice: ");
+    return Console.ReadLine();
+}
+
+static void RouteEm(string userInput){
+    switch (userInput){
+        case "1":
+        PlainPizza();
+        break;
+
+        case "2":
+        CheesePizza();
+        break;
+
+        case "3":
+        PepperoniPizza();
+        break;
+
+        case "4":
+        break;
+
+        default:
+        RetrySequence();
+        break;
+    }
+}
+
+static void PlainPizza()
+    {
+        Random pizza = new Random();
+        int pizzaRow = pizza.Next(8, 13);
+        Random order = new Random();
+        int orderNum = order.Next(0, 150);
+        Console.WriteLine($"Order up for order number {orderNum}");
+
+        for (int i = pizzaRow; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                Console.Write("^ ");
+            }
+            Console.WriteLine();
+        }
+        ToppingKey();
+        Console.WriteLine("Press any key to return to the menu...");
+        Console.ReadKey();
+    }
+
+    static void CheesePizza()
+    {
+        Random pizza = new Random();
+        int pizzaRows = pizza.Next(8, 13); 
+        Random order = new Random();
+        int orderNum = order.Next(0, 150);
+        Console.WriteLine($"Order up for order number {orderNum}");
+        for (int i = pizzaRows; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                if (i == pizzaRows || i == 1 || j == 0 || j == i - 1){
+                    Console.Write("^ ");
+                }
+                else{
+                    Console.Write("# ");
+                }
+            }
+            Console.WriteLine();
+        }
+        ToppingKey();
+        Console.WriteLine("Press any key to return to the menu...");
+        Console.ReadKey();
+    }
+
+    static void PepperoniPizza()
+    {
+        Random pizza = new Random();
+        int pizzaRows = pizza.Next(8, 13);
+        Random order = new Random();
+        int orderNum = order.Next(0, 150);
+        Console.WriteLine($"Order up for order number {orderNum}");
+        for (int i = pizzaRows; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                if (i == pizzaRows || i == 1 || j == 0 || j == i - 1){
+                    Console.Write("^ ");
+                }
+                else{
+                    if (j > 0 && j < i - 1 && pizza.Next(0, 5) == 0){
+                        Console.Write("[] ");
+                    }
+                    else{
+                        Console.Write("# ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+        ToppingKey();
+        Console.WriteLine("Press any key to return to the menu...");
+        Console.ReadKey();
+    }
+
+    static void ToppingKey(){
+        Console.WriteLine("^ = bread\n# = cheese\n[] = pepperoni");
+    }
+    static void ExitSequence()
+    {
+        Console.WriteLine("Goodbye!");
+    }
+
+    static void RetrySequence()
+    {
+        Console.WriteLine("Invalid choice, please try again.");
+        Console.WriteLine("Press any key to return to the menu...");
+        Console.ReadKey();
+    }
